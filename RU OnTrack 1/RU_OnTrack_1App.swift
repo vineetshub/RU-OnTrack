7d67412a373f6682898739671bbeb0f244b29540
vineetshub <vms118@scarletmail.rutgers.edu>
2024-03-23 13:34:11 -0400

    Initial Commit

---
+++ RU OnTrack 1/RU_OnTrack_1App.swift
@@ -0,0 +1,17 @@
+//
+//  RU_OnTrack_1App.swift
+//  RU OnTrack 1
+//
+//  Created by Vineet Shah on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct RU_OnTrack_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
